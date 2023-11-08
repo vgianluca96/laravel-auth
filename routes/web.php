f40f159edbffd34532ce9d04845210aa8822d986
vgianluca96 <gianluca.vallese@hotmail.com>
2023-11-08 18:03:49 +0100

    creata index + rotte, spostato ProjectController
    ProjectController dava problemi, spostato in cartella sup

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +21,10 @@ Route::get('/', function () {
 });
 
 
+
 Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('projects', ProjectController::class);
 });
 
 
