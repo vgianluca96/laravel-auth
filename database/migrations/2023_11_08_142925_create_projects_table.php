d13631679791de1bccc599c2d71d292ccdd69c48
vgianluca96 <gianluca.vallese@hotmail.com>
2023-11-08 16:34:42 +0100

    impostate migration e seeder

--- database/migrations/2023_11_08_142925_create_projects_table.php
+++ database/migrations/2023_11_08_142925_create_projects_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('repo_name')->nullable();
+            $table->string('description')->nullable();
+            $table->string('slug');
+            $table->string('thumb')->nullable();
             $table->timestamps();
         });
     }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,14 +4,26 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Project;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $newproject = new Project();
+            $newproject->title = $faker->realText(20);
+            $newproject->repo_name = $faker->realText(20);
+            $newproject->description = $faker->realText(200);
+            $newproject->slug = Str::slug($newproject->title, '-');
+            dd($faker->image('public/storage/placeholders', 640, 480, 'Posts', false));
+            $newproject->thumb = 'placeholders/' . $faker->image('public/storage/placeholders', 640, 480, 'Posts', false);
+            $newproject->save();
+        }
     }
 }
