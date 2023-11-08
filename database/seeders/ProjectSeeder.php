65769c149e7c5857fdfd86e98d3d44a3027f00ae
vgianluca96 <gianluca.vallese@hotmail.com>
2023-11-08 17:24:04 +0100

    temporaneamente eliminate le img dal seeder
    problemi con faker, temporaneamente le thumb saranno null

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Models\Project;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
@@ -21,8 +22,11 @@ class ProjectSeeder extends Seeder
             $newproject->repo_name = $faker->realText(20);
             $newproject->description = $faker->realText(200);
             $newproject->slug = Str::slug($newproject->title, '-');
-            dd($faker->image('public/storage/placeholders', 640, 480, 'Posts', false));
-            $newproject->thumb = 'placeholders/' . $faker->image('public/storage/placeholders', 640, 480, 'Posts', false);
+            //dd($faker->image('public/storage/placeholders', 640, 480, 'Posts', false));
+            //$path = Storage::getConfig()['root'] . DIRECTORY_SEPARATOR . "placeholders";
+            //echo $faker->image($path, 640, 480, 'Posts') . PHP_EOL;
+            //dd($faker->image('storage/app/public/placeholders', 640, 480, 'Posts', false));
+            //$newproject->thumb = 'placeholders/' . $faker->image('storage/app/public/placeholders', 640, 480, 'Posts', false);
             $newproject->save();
         }
     }
